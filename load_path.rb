2cc702ebe5485586a44787d04dba9aadb30c97ed
Scott Bellware <sbellware@gmail.com>
2018-08-12 22:19:48 -0500

    Load path is corrected

--- load_path.rb
+++ load_path.rb
@@ -8,7 +8,7 @@ rescue LoadError
   Bundler.require
 end
 
-lib_dir = File.expand_path(File.join(__dir__, 'lib'))
+lib_dir = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 libraries_dir = ENV['LIBRARIES_HOME']
